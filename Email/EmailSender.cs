b8ba83de5896170b370ea6cf3f45f0a0532b3788
okan <okqn145@gmail.com>
2024-11-18 23:09:24 +0300

    Add project files.

---
+++ Email/EmailSender.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafe.Email
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(Options.SendGridKey);
+            var mesaj = new SendGridMessage()
+            {
+                From = new EmailAddress("okqn145@gmail.com","Yeni Restaurant"),
+                Subject=subject,
+                PlainTextContent=htmlMessage,
+                HtmlContent=htmlMessage
+            };
+            mesaj.AddTo(new EmailAddress(email));
+            try
+            {
+                return client.SendEmailAsync(mesaj);
+            }
+            catch(Exception)
+            { 
+                throw;
+            }
+            return null;
+        }
+        public EmailOptions Options { get; set; }
+        public EmailSender(IOptions<EmailOptions> emailOptions)
+        {
+            Options = emailOptions.Value;
+        }
+    }
+}
